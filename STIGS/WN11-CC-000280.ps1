2a4b00f69fa1e9b706eae5fd506cf9173c972d43
Brian Felton <bfelton305@gmail.com>
2026-03-21 09:40:30 -0400

    Create  WN11-CC-000280.ps1

---
+++ STIGS/WN11-CC-000280.ps1
@@ -0,0 +1,37 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the fPromptForPassword = 1 → Forces Remote Desktop (RDP) to always prompt for a password, improving authentication security.
+
+.NOTES
+    Author          : Brian Felton
+    LinkedIn        : linkedin.com/in/brian-felton-22b2b03a8/
+    GitHub          : github.com/bfelton786
+    Date Created    : 2026-03-21
+    Last Modified   : 2026-03-21
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         :  WN11-CC-000280
+
+.TESTED ON
+    Date(s) Tested  : 2026-03-21
+    Tested By       : 2026-03-21
+    Systems Tested  : bfelton-stig-vm
+    PowerShell Ver. : PowerShell ISE
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID- WN11-CC-000280.ps1 
+#>
+
+# Define the registry path and value
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Force | Out-Null
+
+# Set fPromptForPassword to 1
+Set-ItemProperty `
+  -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" `
+  -Name "fPromptForPassword" `
+  -Value 1 `
+  -Type DWord `
+  -Force
